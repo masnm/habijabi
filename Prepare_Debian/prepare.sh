85de059cdbbe960c84d4f0af523fd2230b82c3ec
masnm <49545838+masnm@users.noreply.github.com>
2022-02-25 01:28:12 +0600

    Update prepare.sh

--- Prepare_Debian/prepare.sh
+++ Prepare_Debian/prepare.sh
@@ -9,69 +9,15 @@ localectl status
 timedatectl
 
 # upgrading the system
-sudo apt-get update
-sudo apt update
-sudo apt upgrade
+sudo apt-get update && sudo apt update && sudo apt upgrade
 
 # cleanup after upgrade
 sudo apt autoremove
 
-## its a good idea to reboot the system now
-
-# cheaking for update again
-sudo apt update
+# need to add backport first
 
 # android auto mounter
-sudo apt install gvfs-backends
+sudo apt install gvfs-backends vim git lxappearance pavucontrol compton uget xclip i3lock-fancy materia-gtk-theme papirus-icon-theme gcc g++ gdb maim unzip tmux tree fonts-lohit-beng-bengali celluloid
 
-# editor of choice 'vim'
-sudo apt install vim
-
-# installing git
-sudo apt install git
-
-# install theme controller
-sudo apt install lxappearance
-
-# audio control manager
-sudo apt install pavucontrol
-
-# compton compositor
-sudo apt install compton
-
-# installing download manager
-sudo apt install uget
-
-# clipboard manager
-sudo apt install xclip
-
-# find some good loking lock
-sudo apt install i3lock-fancy
-
-# theme of choice for icon and gtk
-sudo apt install materia-gtk-theme papirus-icon-theme
-
-# C and C++ compiler and debugger
-sudo apt install gcc g++ gdb
-
-# Screenshot package
-sudo apt install maim
-
-# Unzipping package
-sudo apt install unzip
-
-# terminal multiplexer
-sudo apt install tmux
-
-# tree package to explore sub-directoryes
-sudo apt install tree
-
-# Bangla fonts
-sudo apt-get install fonts-beng fonts-beng-extra fonts-lohit-beng-bengali
-
-# Multimedia Player
-sudo apt install celluloid
-
-# need to add backport first
 # Installing the LibreOffice Swit 'gtk3' rendering
 sudo apt install -t bullseye-backports libreoffice libreoffice-gtk3
