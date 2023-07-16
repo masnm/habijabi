305f0af8aa6fb829fe1a69277d9a060d0db48c09
masnm <49545838+masnm@users.noreply.github.com>
2023-07-16 11:29:45 -0400

    Update packages.sh

--- configs/packages.sh
+++ configs/packages.sh
@@ -7,7 +7,123 @@ zsh-autosuggestions \
 zsh-syntax-highlighting \
 autojump-zsh \
 vim \
-git
+git \
+git \
+xclip \
+xfce4-power-manager-settings \
+rxvt-unicode \
+materia-gtk-theme \
+papirus-icon-theme \
+lxappearance \
+git \
+zsh \
+zsh-autosuggestions \
+zsh-syntax-highlighting \
+vim \
+xclip \
+i3blocks \
+thunar \
+thunar-volman \
+xreader \
+gcc \
+g++ \
+clang \
+clang-tools-extra \
+gvfs-mtp \
+blueman \
+bluez \
+bluez-tools \
+xreader \
+transmission \
+vlc \
+feh \
+g++ \
+gcc \
+gdb \
+git \
+lxappearance \
+maim \
+make \
+materia-gtk-theme \
+mousepad \
+papirus-icon-theme \
+pavucontrol \
+thunar \
+tmux \
+transmission \
+tree \
+ctags \
+unzip \
+vim \
+xclip \
+blueman \
+bluez \
+bluez-utils \
+ctags \
+feh \
+gcc \
+gdb \
+git \
+lightdm-gtk-greeter-settings \
+lxappearance \
+maim \
+make \
+materia-gtk-theme \
+mousepad \
+papirus-icon-theme \
+pavucontrol \
+thunar \
+thunar-volman \
+tmux \
+tree \
+unzip \
+vim \
+xclip \
+xdg-user-dirs
+
+
+
+
+compton
+acpi
+fontawesome-fonts
+sysstat
+xset
+playerctl
+xreader
+transmission
+vlc
+nitrogen
+uget
+bash-completion
+brightnessctl
+gnome-disk-utility
+gvfs
+gvfs-mtp
+libinput
+libnotify
+lsd
+man
+notification-daemon
+noto-fonts-emoji
+playerctl
+thunar
+thunar-volman
+tmux
+tree
+unzip
+vim
+xclip
+xf86-input-libinput
+xorg
+xorg-xinit
+xorg-xmodmap
+xorg-xrdb
+xss-lock
+xterm
+
+
+
 
 # setting zsh as default shell
 chsh -s $(which zsh)
